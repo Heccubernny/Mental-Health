7bee26e832d444998b1394b885ff5b0465c7cd91
Ikubanni Paul <ipom4eva@gmail.com>
2022-07-20 14:27:58 +0100

    Uploading will work on the readme file during my leisure time

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from "../components/Layout/Layout"
+export default function Home(){
+  return(
+    <div>
+      <Head>
+        <title>MHDI</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      </Head>
+      <Layout>
+      
+      </Layout>
+    </div>
+    
+  )
+}
